20d2c33dd7d81219a754daec5498eb3f3fffe6cd
Aman Almadi <amanalmadiofficial@gmail.com>
2025-02-24 17:18:15 +0530

    Added enemy AI and player attack system

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    private Transform player;
+    private Vector3 wanderTarget;
+    private Animator animator;
+
+    public float detectionRange = 7f;
+    public float wanderRadius = 10f;
+    private bool isChasing = false;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        SetRandomWanderTarget();
+    }
+
+    private void Update()
+    {
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+        if (distanceToPlayer < detectionRange)
+        {
+            isChasing = true;
+
+            agent.SetDestination(player.position);
+        }
+        else if (isChasing)
+        {
+            isChasing = false;
+
+            SetRandomWanderTarget();
+        }
+
+        if (!isChasing && agent.remainingDistance < 0.5f)
+        {
+            SetRandomWanderTarget();
+        }
+
+        animator.SetBool("isWalking", agent.velocity.magnitude > 0.1f);
+    }
+
+    private void SetRandomWanderTarget()
+    {
+        Vector3 randomDirection = Random.insideUnitSphere * wanderRadius;
+
+        randomDirection += transform.position;
+
+        NavMeshHit hit;
+
+        if (NavMesh.SamplePosition(randomDirection, out hit, wanderRadius, NavMesh.AllAreas))
+        {
+            wanderTarget = hit.position;
+
+            agent.SetDestination(wanderTarget);
+        }
+    }
+}
