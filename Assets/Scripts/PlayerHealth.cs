19a6da9618c6fbbdc4ee07a9dbfec24d3576b20a
Aman Almadi <amanalmadiofficial@gmail.com>
2025-02-24 18:31:06 +0530

    Added player damage animation & hurt effect

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,22 +1,28 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class PlayerHealth : MonoBehaviour
 {
     public int maxHealth = 100;
+    public float hurtDuration = 0.2f;
     private int currentHealth;
     public Slider healthBar;
     public GameObject gameOverScreen;
     private Animator animator;
+    private SpriteRenderer spriteRenderer;
+    private Color originalColor;
     private bool isDead = false;
 
     private void Start()
     {
-        currentHealth = maxHealth;
-        animator = GetComponent<Animator>();
-        healthBar.maxValue = maxHealth;
-        healthBar.value = currentHealth;
+        currentHealth = maxHealth;  
+
+        UpdateHealthBar();
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        originalColor = spriteRenderer.color;
     }
 
     public void TakeDamage(int damage)
@@ -24,7 +30,10 @@ public class PlayerHealth : MonoBehaviour
         if (isDead) return;
 
         currentHealth -= damage;
-        healthBar.value = currentHealth;
+
+        animator.SetTrigger("Hurt");
+        UpdateHealthBar();
+        StartCoroutine(HurtEffect());
 
         if (currentHealth <= 0)
         {
@@ -43,5 +52,21 @@ public class PlayerHealth : MonoBehaviour
     public void RestartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    private void UpdateHealthBar()
+    {
+        healthBar.maxValue = maxHealth;
+        healthBar.value = currentHealth;
+        animator = GetComponent<Animator>();
+    }
+
+    IEnumerator HurtEffect()
+    {
+        spriteRenderer.color = Color.red;
+
+        yield return new WaitForSeconds(hurtDuration);
+        
+        spriteRenderer.color = originalColor;
     }
 }
